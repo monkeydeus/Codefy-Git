050aee33a358596bf3da5ae5dbecc1bfeb3d5f3d
Joe Kraft <Joe.Kraft@neudesic.com>
2023-04-19 14:05:53 -0700

    Added State model file

---
+++ Codefy-API/State.cs
@@ -0,0 +1,10 @@
+﻿namespace Codefy_API;
+
+public class State
+{
+    public string Name { get; set; }
+    public string? Capital { get; set; }
+    public int? Population { get; set; }
+    public int  YearFounded { get; set; }
+    public string Motto { get; set; }
+}
